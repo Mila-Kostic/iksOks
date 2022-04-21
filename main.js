8d65398bf4710b759ff4d941ac8c490143681363
Mila-Kostic <milakostic.eng@gmail.com>
2022-04-21 19:09:58 +0200

    iksOks

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert('Happy codding!')
+// alert('Happy codding!');
+
+let container = document.querySelector('.container');
+let symbol = "X";
+
+createGrid();
+
+let boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box=>{
+    box.addEventListener('click',insertSymbol)
+})
+
+function createGrid() {
+    let text = '';
+    for (let i = 0; i < 9; i++) {
+        text += '<div class="box"></div>'
+        
+    }
+    container.innerHTML = text;
+}
+
+function insertSymbol(){
+    //this.innerHTML = "X";
+    this.removeEventListener('click', insertSymbol);
+    (symbol === "X") ? symbol = "0" : symbol = "X";
+    this.innerHTML = symbol;
+}
