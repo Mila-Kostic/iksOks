c1686cb4133bf8a8323dff23d21f105ddd42c9e7
Mila Kostic <milakostic.eng@gmail.com>
2022-07-25 22:00:36 +0200

    checkWins

--- main.js
+++ main.js
@@ -16,8 +16,20 @@ function insertSymbol() {
   this.removeEventListener("click", insertSymbol);
   symbol === "X" ? (symbol = "0") : (symbol = "X");
   this.innerHTML = symbol;
+  checkWins();
 }
-
+function checkWins() {
+  if (
+    boxes[0].innerHTML === boxes[1].innerHTML &&
+    boxes[0].innerHTML === boxes[2].innerHTML &&
+    boxes[0].innerHTML !== ""
+  ) {
+    console.log("Pogodak");
+    boxes[0].style.background = "tomato";
+    boxes[1].style.background = "tomato";
+    boxes[2].style.background = "tomato";
+  }
+}
 function createGrid() {
   let text = "";
   for (let i = 0; i < 9; i++) {
